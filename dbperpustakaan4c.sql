af857307c1cc844f21c50dca22bf862c9ae01371
adityaikhwandi <adityapohan3219@gmail.com>
2024-05-18 21:04:49 +0700

    first commit

---
+++ dbperpustakaan4c.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 18 Bulan Mei 2024 pada 14.51
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbperpustakaan4c`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(8) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(205) NOT NULL,
+  `jkl` enum('pria','wanita') NOT NULL,
+  `agama` varchar(25) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `pwd` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `alamat`, `jkl`, `agama`, `email`, `pwd`, `created_at`, `updated_at`) VALUES
+('22220678', 'ratu clara', 'jampalan', 'wanita', 'BUDHA', 'ratuindi@gmail.com', '3123', '2024-05-18 19:13:06', '2024-05-18 19:13:06'),
+('22225461', 'febrina aulya', 'sentang', 'wanita', 'ISLAM', 'febrina@gmail.com', '8923', '2024-05-18 19:45:38', '2024-05-18 19:45:38');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `idpinjam` varchar(28) NOT NULL,
+  `idmhs` varchar(9) NOT NULL,
+  `tglpinjam` datetime NOT NULL,
+  `tglkembali` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`idpinjam`),
+  ADD UNIQUE KEY `idmhs` (`idmhs`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
